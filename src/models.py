bc2e62f169b699fc0624f298a615645bb444c63f
javigarzon1 <javi.garzon@hotmail.com>
2025-03-14 19:58:53 +0000

    tt

--- src/models.py
+++ src/models.py
@@ -1,7 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy, ForeingKey
-from sqlalchemy import String, Integer, SQLAlchemy
-from sqlalchemy.orm import Column
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer, ForeignKey, Enum
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -14,28 +13,56 @@ class Usuario(db.Model):
     secondname = Column(String(50), nullable=False)
     email = Column(String(50), unique=True, nullable=False)
 
-def serialize(self):
+    comments = relationship('Comment', backref='user', lazy=True)
+
+    def serialize(self):
         return {
             "id": self.id,
             "username": self.username,
             "firstname": self.firstname,
-             "firstname": self.firstname,
-             "email": self.email,
+            "secondname": self.secondname,
+            "email": self.email,
         }
 
 class Comment(db.Model):
     __tablename__ = 'comment'
+    
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(200), nullable=False)
-    author_id = Column(Integer, db.ForeignKey('user.id'), nullable=False)
-    post_id = Column()
+    author_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)  # Se asume que existe un modelo Post
+    
+    comments = relationship('Comentario', backref='post', lazy=True)
 
     def serialize(self):
         return {
             "id": self.id,
             "comment_text": self.comment_text,
             "author": self.author_id, 
-             "post": self.post_id, 
+            "post": self.post_id, 
+        }
+class Comentario (db.Model):
+    __tablename__ = 'post'  
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
+
+def serialize(self):
+        return {
+            "id": self.id,
+            "id": self.user_id,            
         }
 
-comments = relationship('comment', backref='user', lazy=True)
+class Media (db.Model):
+    __tablename__ = 'Media'  
+    id = Column(Integer, primary_key=True)
+    type= Column(Enum, nullable=False)
+    url = Column(String(100), nullable=False)
+    post_id = Column(Integer, ForeignKey('Post.id'), nullable=False)
+
+def serialize(self):
+        return {
+            "id": self.id,
+            "type": self.type, 
+            "url": self.url,
+            "post": self.post,           
+        }
