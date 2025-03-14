74d35064dac398bddedbc2b2ed26ae56f1c2edc8
javigarzon1 <javi.garzon@hotmail.com>
2025-03-14 12:10:28 +0000

    n

--- src/models.py
+++ src/models.py
@@ -1,19 +1,41 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from flask_sqlalchemy import SQLAlchemy, ForeingKey
+from sqlalchemy import String, Integer, SQLAlchemy
+from sqlalchemy.orm import Column
+
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class Usuario(db.Model):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True)
+    firstname = Column(String(50), nullable=False)
+    secondname = Column(String(50), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
 
+def serialize(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+             "firstname": self.firstname,
+             "email": self.email,
+        }
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(200), nullable=False)
+    author_id = Column(Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = Column()
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "comment_text": self.comment_text,
+            "author": self.author_id, 
+             "post": self.post_id, 
         }
+
+comments = relationship('comment', backref='user', lazy=True)
